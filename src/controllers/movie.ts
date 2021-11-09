8af5ccdadd39bbb22662f6c543f5cd63c5c1f109
adedayojs <samfeolu@gmail.com>
2021-11-09 14:43:37 +0100

    Update movie controller to fetch from swapi api

--- src/controllers/movie.ts
+++ src/controllers/movie.ts
@@ -1,14 +1,18 @@
 import { User } from '@models/User';
 import got from 'got';
+import { swapiBaseMovieUrl } from '~/helpers/constant';
 const MovieController = {
   /**
    *
    * Display All Movies
    */
-  show() {
+  showAll() {
     // Make APi Call to external api
-    got('');
-    return User.find();
+    return got(swapiBaseMovieUrl, { responseType: 'json' }).then((res) => res.body);
+  },
+  show(id: number) {
+    // Make APi Call to external api
+    return got(`${swapiBaseMovieUrl}/${id}`, { responseType: 'json' }).then((res) => res.body);
   },
 };
 export default MovieController;
--- src/routes/v1/movie.ts
+++ src/routes/v1/movie.ts
@@ -3,9 +3,18 @@ import Controller from '@controllers/movie';
 
 const router = Router();
 
+router.get('/:id', async function (req, res, _next) {
+  // Fetch from datasource, in this case external api
+  const { id } = req.params;
+  const movies = await Controller.show(Number(id));
+
+  // Sort based on parameters requested
+  res.status(200).json(movies);
+});
+
 router.get('/', async function (_req, res, _next) {
   // Fetch from datasource, in this case external api
-  const movies = await Controller.show();
+  const movies = await Controller.showAll();
 
   // Sort based on parameters requested
   res.status(200).json(movies);
